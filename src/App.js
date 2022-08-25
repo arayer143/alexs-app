30df2727d7edfbced5354ea6cbdf5c19517b2d57
arayer143 <89434892+arayer143@users.noreply.github.com>
2022-08-25 15:53:43 -0500

    React App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Alex Rayer App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
